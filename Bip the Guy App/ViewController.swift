d9d4ad55218ea724aae1b63a14e4840c20119139
Kelsey Bishop <kelseybishop@Kelseys-MacBook-Air.local>
2017-09-19 15:46:49 -0400

    Bip guy app

--- Bip the Guy App/ViewController.swift
+++ Bip the Guy App/ViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    
+    @IBOutlet weak var imageToPunch: UIImageView!
+    var audioPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,75 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: functions
+    
+    func animateImage() {
+        let bounds = self.imageToPunch.bounds
+        let shrinkValue: CGFloat = 60
 
+        // shrink our imageToPunch by 60 pixels
+        self.imageToPunch.bounds = CGRect(x: self.imageToPunch.bounds.origin.x + shrinkValue, y: self.imageToPunch.bounds.origin.y + shrinkValue, width: self.imageToPunch.bounds.size.width - shrinkValue, height: self.imageToPunch.bounds.size.height - shrinkValue)
+        
+        UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options: [], animations: { self.imageToPunch.bounds = bounds }, completion: nil)
+    }
+    
+    func playSound(soundName: String, audioPlayer: inout AVAudioPlayer) {
+        // Can we load in the file called soundName?
+        if let sound = NSDataAsset(name: soundName){
+            // check if sound.data is a sound file
+            do{
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                // if sound.data is not a valid audio file
+                print("ERROR: data is \(soundName) couldn't be played as a sound.")
+            }
+        } else {
+            // if reading in the NSDataAsset didn't work, tell the user / report the error.
+            print("ERROR: file\(soundName) didn't load.")
+        }
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func libraryPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func cameraPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
+        animateImage()
+        playSound(soundName: "punchSound", audioPlayer: &audioPlayer)
+    }
+    
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
